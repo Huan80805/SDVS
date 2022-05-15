e122457892cb210319cbae2ccf10754799aafed9
huan80805 <jason101805@gmail.com>
2022-05-15 15:24:17 +0800

    clean files

--- test_pe.py
+++
@@ -1,8 +0,0 @@
-import mediapipe as mp
-import cv2
-from alg import pe
-if __name__ == "__main__":
-    test_img = cv2.imread("pose.jpeg")
-    test_img = pe(test_img)
-    cv2.imwrite("pose.jpeg")
-
